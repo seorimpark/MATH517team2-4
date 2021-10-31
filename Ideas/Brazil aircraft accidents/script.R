078ff60400e2af16b74cd72ccf19dbe0edcc4622
FahimBeck <43608481+FahimBeck@users.noreply.github.com>
2021-10-31 11:53:01 +0100

    Idea about Brazilian aeronautics accidents

---
+++ Ideas/Brazil aircraft accidents/script.R
@@ -0,0 +1,33 @@
+
+data = read.csv("aircrafts.csv")
+names(data)
+dim(data)
+max(table(data$model))
+table(data$type_operation)
+
+data2 = read.csv("occurrences.csv")
+names(data2)
+unique(data2$fu)
+
+#################################
+
+install.packages("geobr")
+library(geobr)
+library(ggplot2)
+
+df <- utils::read.csv(system.file("extdata/br_states_lifexpect2017.csv", package = "geobr"), encoding = "UTF-8")
+
+states <- read_state(year=2019)
+
+states$name_state <- tolower(states$name_state)
+df$uf <- tolower(df$uf)
+
+# join the databases
+states <- dplyr::left_join(states, df, by = c("name_state" = "uf"))
+
+ggplot() +
+  geom_sf(data=states, aes(fill=ESPVIDA2017), color= NA, size=.15) +
+  labs(subtitle="Life Expectancy at birth, Brazilian States, 2014", size=8) +
+  scale_fill_distiller(palette = "Blues", name="Life Expectancy", limits = c(65,80)) +
+  theme_minimal() +
+  theme_void()
